df7403ff8aa5cae860472fd70aa89a8d99b2c51d
eduardomingoranca <eduardomingoranca@gmail.com>
2020-04-15 13:36:00 -0300

    Criação do endPoint getAllPostagens

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FeedComponent } from './feed/feed.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   // declarations --> declara todos os componentes 
@@ -21,7 +22,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /*
+  CRUD - Create, Ready, Update e Delete
+
+  Ready utiliza-se o método >> get
+  Create utiliza-se o método >> post
+  Update utiliza-se o método >> put
+  Delete utiliza-se o método >> delete
+
+  */
+
+  // ready
+  getAllPostagens() {
+    return this.http.get('http://31.220.57.14:8080/postagens')
+  } // getAllPostagens >> lista todas as postagens no servidor
+
 }
