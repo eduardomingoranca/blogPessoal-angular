0f84ab0029035edb91bf11ac7afcd7ce24a541ba
eduardomingoranca <eduardomingoranca@gmail.com>
2020-04-22 14:14:46 -0300

    Implementação do getAllPostagens no componente Feed

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostagemService } from '../service/postagem.service';
+import { Postagem } from '../model/Postagem';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  // estanciando a class postagem 
+  /* estancia/chama a class postagem para 
+  saber quais são os dados, os campos e atributos
+  que serão necessários. */
+  listaPostagens: Postagem[];
 
-  ngOnInit(): void {
+
+  // importando os módulos
+  /* importar todos os módulos, essa importação,
+  é chamada de injeção de depêndencia  */
+  constructor(private postagemService: PostagemService) { }
+
+  // ngOnInit(): void {
+  // }
+  /* ngOnInit() >> é um método que todos os códigos que estiverem dentro dele serão carregados,
+  quando a aplicação for iniciada.  */
+
+  ngOnInit() {
+    this.findAllPostagens();
   }
 
+  // criando um método/função
+  findAllPostagens() {
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp;
+    });
+  }
+  /* findAllPostagens() >> é um método que traz todas as postagens que 
+  estão registradas, ele fará isso através do postagemService. O postagemService
+  irá trazer isso através do endPoint. */
+
+  /* subscribe >> uma palavra reservada para trazer um array, e
+  inserir dentro da lista de postagens. */
+
+
 }
+
